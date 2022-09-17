81e486664f05b86a295c3b66153ece326f26b87d
Wes PC <wesleyoliveirap@gmail.com>
2022-09-17 12:06:35 -0300

    Main Camera and Gameviewport

--- core/src/scenes/GamePlay.java
+++ core/src/scenes/GamePlay.java
@@ -1,20 +1,43 @@
 package scenes;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mygdx.jackthegiant.GameMain;
+
+import helpers.GameInfo;
 
 public class GamePlay implements Screen {
 
     private GameMain game;
+    private OrthographicCamera mainCamera;
+    private Viewport gameViewport;
     private Sprite[] bgs;
 
     public GamePlay(GameMain game) {
         this.game = game;
 
+        this.mainCamera = new OrthographicCamera(GameInfo.WIDTH, GameInfo.HEIGHT);
+        this.mainCamera.position.set(GameInfo.WIDTH / 2f, GameInfo.HEIGHT / 2f, 0);
+
+        this.gameViewport = new StretchViewport(GameInfo.WIDTH, GameInfo.HEIGHT, this.mainCamera);
+
+
+
         this.createBackgrounds();
+    }
+
+    void update(float dt) {
+        moveCamera();
+    }
+
+    void moveCamera() {
+        this.mainCamera.position.y -= 1;
     }
 
     void createBackgrounds() {
@@ -39,6 +62,9 @@ public class GamePlay implements Screen {
 
     @Override
     public void render(float delta) {
+
+        this.update(delta);
+
         ScreenUtils.clear(1, 0, 0, 1);
 
         this.game.getBatch().begin();
@@ -46,6 +72,9 @@ public class GamePlay implements Screen {
         this.drawBackgrounds();
 
         this.game.getBatch().end();
+
+        this.game.getBatch().setProjectionMatrix(this.mainCamera.combined);
+        this.mainCamera.update();
     }
 
     @Override
